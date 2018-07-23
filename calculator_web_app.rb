7db3acad1eb82b9fc867ede9c8340184101f6512
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-23 12:08:07 -0400

    Corrected some errors on first page

--- calculator_web_app.rb
+++ calculator_web_app.rb
@@ -1,35 +1,32 @@
 require "sinatra"
 get '/' do
-	erb :calculator_app_page1
+	erb :calculator_app_page1, locals:{error: ""}
 end
 post '/calculator_app_page1' do
 	user_name_input = params[:user_name_input]
 	password_input = params[:password_input]
-user_name1 = ["michaelsmith19951"]
-user_name2 = ["msmith61095"] 
-user_name3 = ["mikes1995"]
-password1 = ["Mike1995"]
-password2 = ["m@ggie123!"] 
-password3 = ["bell@123!"]
-if user_name_input == "michaelsmith19951" && password_input == "Mike1995"
-	redirect '/calculator_app_page2?user_name_input=' + user_name_input + '&password_input=' + password_input
-else
-	p "Incorrect login. Please try again."
-
-if user_name_input == "msmith61095" && password_input == "m@ggie123"
-	p "Login successful!"
-else
-	p "Incorrect login. Please try again."
-
-if user_name_input == "mikes1995" && password_input == "bell@123!"
-	p "Login successful!"
-else
-	p "Incorrect login. Please try again."
+	first_name_input = params[:first_name_input]
+	last_name_input = params[:last_name_input]
+user_arr = ["michaelsmith19951", "msmith61095", "mikes1995"]
+password_arr = ["Mike1995", "m!ke123!", "ms1995!"]
+counter = 0
+user_arr.each do |username|
+	if username == user_name_input && password_input == password_arr[counter]
+		redirect '/calculator_app_page2?'
+	end
+	counter = counter + 1
 end
+unless user_arr.include?(user_name_input)
+	erb :calculator_app_page1, locals:{error: "The username is incorrect. Please try again."}
 end
-end
+	erb :calculator_app_page1, locals:{error: "The password is incorrect. Please try again."}
 end
 get '/calculator_app_page2' do
+	first_name_input = params[:first_name_input]
+	last_name_input = params[:last_name_input]
+	erb :calculator_app_page2, locals:{first_name_input: params[first_name_input], last_name_input: params[:last_name_input]}
+end
+post '/calculator_app_page2' do
 	first_name_input = params[:first_name_input]
 	last_name_input = params[:last_name_input]
 	erb :calculator_app_page2, locals:{first_name_input: params[first_name_input], last_name_input: params[:last_name_input]}
