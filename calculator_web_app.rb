ec6cd556e6ba7b65913670b7faeaee0b18e21deb
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-23 15:36:22 -0400

    Finished second page, started and finished third page

--- calculator_web_app.rb
+++ calculator_web_app.rb
@@ -7,19 +7,19 @@ post '/calculator_app_page1' do
 	password_input = params[:password_input]
 	first_name_input = params[:first_name_input]
 	last_name_input = params[:last_name_input]
-user_arr = ["michaelsmith19951", "msmith61095", "mikes1995"]
-password_arr = ["Mike1995", "m!ke123!", "ms1995!"]
-counter = 0
-user_arr.each do |username|
-	if username == user_name_input && password_input == password_arr[counter]
-		redirect '/calculator_app_page2?'
+	user_arr = ["michaelsmith19951", "msmith61095", "mikes1995"]
+	password_arr = ["Mike1995", "m!ke123!", "ms1995!"]
+	counter = 0
+	user_arr.each do |username|
+		if username == user_name_input && password_input == password_arr[counter]
+			redirect '/calculator_app_page2?'
+		end
+		counter = counter + 1
 	end
-	counter = counter + 1
-end
-unless user_arr.include?(user_name_input)
-	erb :calculator_app_page1, locals:{error: "The username is incorrect. Please try again."}
-end
-	erb :calculator_app_page1, locals:{error: "The password is incorrect. Please try again."}
+	unless user_arr.include?(user_name_input)
+		erb :calculator_app_page1, locals:{error: "The username is incorrect. Please try again."}
+	end
+		erb :calculator_app_page1, locals:{error: "The password is incorrect. Please try again."}
 end
 get '/calculator_app_page2' do
 	first_name_input = params[:first_name_input]
@@ -29,6 +29,41 @@ end
 post '/calculator_app_page2' do
 	first_name_input = params[:first_name_input]
 	last_name_input = params[:last_name_input]
-	erb :calculator_app_page2, locals:{first_name_input: params[first_name_input], last_name_input: params[:last_name_input]}
+	redirect '/calculator_app_page3?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input
 end
-
+get '/calculator_app_page3' do
+	first_name_input = params[:first_name_input]
+	last_name_input = params[:last_name_input]
+	num1 = params[:num1]
+	num2 = params[:num2]
+	math_operation = params[:math_operation]
+	erb :calculator_app_page3, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], num1: params[:num1], num2: params[:num2], math_operation: params[:math_operation]}
+end
+post '/calculator_app_page3' do
+	puts "in post '/calculator_app_page3' do params is #{params}"
+	'{"first_name_input"=>"Michael", "last_name_input"=>"Smith", "num1"=>"3", "num2"=>"5", "math_operation"=>"+"}'
+	first_name_input = params[:first_name_input]
+	last_name_input = params[:last_name_input]
+	num1 = params[:num1]
+	num2 = params[:num2]
+	math_operation = params[:math_operation]
+	redirect '/calculator_app_final_page?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input + '&num1=' + num1 + '&num2=' + num2 + '&math_operation=' + math_operation
+end
+get '/calculator_app_final_page' do
+	first_name_input = params[:first_name_input]
+	last_name_input = params[:last_name_input]
+	num1 = params[:num1]
+	num2 = params[:num2]
+	math_operation = params[:math_operation]
+	result = params[:result]
+	erb :calculator_app_final_page, locals:{first_name_input: params[:first_name_input], last_name_input: params[:last_name_input], num1: params[:num1], num2: params[:num2], math_operation: params[:math_operation], result: params[:result]}
+end
+post '/calculator_app_final_page' do
+	first_name_input = params[:first_name_input]
+	last_name_input = params[:last_name_input]
+	num1 = params[:num1]
+	num2 = params[:num2]
+	math_operation = params[:math_operation]
+	result = params[:result]
+	redirect '/calculator_app_page3?first_name_input=' + first_name_input + '&last_name_input=' + last_name_input
+end
